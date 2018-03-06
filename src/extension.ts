7033a55a691c5df2833873d8dd9a0c8b0e1d3935
Darren ha <nberserk@gmail.com>
2018-03-06 23:16:08 +0900

    del strict

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
@@ -17,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
+        // Display a message box to the user 
         vscode.window.showInformationMessage('Hello World!');
     });
 
